614b6898ec47ad25760647fcb69253771e4ca697
Laura SCHAEFER <lschaefer@cfai-formation.fr>
2021-11-10 22:07:48 +0100

    tp04

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/TP02'),
+      dir: require('path').join(__dirname, './coverage/TP04'),
       subdir: '.',
       reporters: [
         { type: 'html' },
